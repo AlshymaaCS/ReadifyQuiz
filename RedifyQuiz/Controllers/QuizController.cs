c2e6bea66de3ab4928968a1e437b159b64535d96
Alshymaa <shamscs85@gmail.com>
2017-12-08 13:32:49 +1000

    Creating project with api/token

---
+++ RedifyQuiz/Controllers/QuizController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace RedifyQuiz.Controllers
+{
+
+    public class QuizController : ApiController
+    {
+        /// <summary>
+        /// Gets readify token
+        /// </summary>
+        /// <returns>Token string</returns>
+        [Route("api/Token")]
+        [HttpGet]
+        public string GetToken()
+        {
+            return "13e75793-25bc-409f-9fe7-c65498762140";
+        }
+
+
+    }
+}
