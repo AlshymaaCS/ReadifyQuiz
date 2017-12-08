2653981f8d1629c757ebd63d6bacddf83a37135d
Alshymaa <shamscs85@gmail.com>
2017-12-08 18:14:47 +1000

    fixing issues

--- RedifyQuiz/Utils/Utilities.cs
+++ RedifyQuiz/Utils/Utilities.cs
@@ -10,7 +10,7 @@ namespace RedifyQuiz.Utils
         /// </summary>
         /// <param name="n"></param>
         /// <returns>integer</returns>
-        public static int NthFibonacciNumber(int n)
+        public static int NthFibonacciNumber(double n)
         {
             int a = 0;
             int b = 1;
