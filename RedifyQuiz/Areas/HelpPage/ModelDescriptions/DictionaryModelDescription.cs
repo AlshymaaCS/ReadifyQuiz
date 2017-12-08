120064494b0c646b871cb3b859a355dbcf68783a
Alshymaa <Alshymaa@DESKTOP-7DOJCRM>
2017-12-08 12:37:00 +1000

    Add project files.

---
+++ RedifyQuiz/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RedifyQuiz.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RedifyQuiz/Areas/HelpPage/ModelDescriptions/ModelDescription.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace RedifyQuiz.Areas.HelpPage.ModelDescriptions
+{
+    /// <summary>
+    /// Describes a type model.
+    /// </summary>
+    public abstract class ModelDescription
+    {
+        public string Documentation { get; set; }
+
+        public Type ModelType { get; set; }
+
+        public string Name { get; set; }
+    }
+}
